27c25cfff869aba338c57dd42ac3a12972677709
Nataliya Kim <magistr1982@gmail.com>
2023-06-29 20:07:11 +0300

    Add basename

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ const App = () => {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AuthLayout>
-          <BrowserRouter basename="/goit-react-hw-09-phonebook">
+          <BrowserRouter>
+          {/* basename="/goit-react-hw-09-phonebook" */}
             <div className="App">
               <Navbar />
               <UserRoutes />
